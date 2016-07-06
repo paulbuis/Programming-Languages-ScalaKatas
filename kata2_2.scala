e32b4d1bc9f5cd62da841a2689ef0f50cf6196f5
Paul Buis <00pebuis@bsu.edu>
2016-07-06 08:05:48 -0400

    Fixed another syntax error

--- kata2_2.scala
+++ kata2_2.scala
@@ -1,6 +1,6 @@
 object Kata3_0 {
 
-    def fib(n : UInt) = {
+    def fib(n : Int) = {
         if (n==0) BigInt(0) else if (n==1) BigInt(1) else fib(n-1)+fib(n-2);
     }
     def main(args: Array[String]) : Unit = {
