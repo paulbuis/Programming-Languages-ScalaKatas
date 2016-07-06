d4cdb4fa7489a7450a44cfdb4cc3713fb33b6d07
Paul Buis <00pebuis@bsu.edu>
2016-07-06 08:12:59 -0400

    Initial Commit

---
+++ kata4_1.scala
@@ -0,0 +1,23 @@
+object Kata4_1 {
+
+    def fib(n : Int) : BigInt = {
+        var a = BigInt(0);
+        var b = BigInt(1);
+        var i = 0;
+        while (i<n) {
+            var t = a;
+            a = b;
+            b = a + t;
+            i = i+1;
+        }
+        a;
+    }
+    
+    def main(args: Array[String]) : Unit = {
+        println(fib(0));
+        println(fib(1));
+        println(fib(2));
+        println(fib(3));
+        println(fib(4));   
+    }
+}
