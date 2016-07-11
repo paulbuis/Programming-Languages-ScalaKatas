04ce69cfc5fdabf8a2174091a11de25658312c0d
Paul Buis <00pebuis@bsu.edu>
2016-07-11 15:45:38 -0400

    Removed non-idiomatic semicolons

--- kata1_1.scala
+++ kata1_1.scala
@@ -1,10 +1,10 @@
 object Kata1_1 {
     
     def main(args: Array[String]): Unit = {
-        val readInt : () => Int = scala.io.StdIn.readInt;
-        val x : Int = readInt();
-        val y : Int = readInt();
-        val z : Int = readInt(); 
-        println( (x+y+z)/3.0 );
+        val readInt : () => Int = scala.io.StdIn.readInt
+        val x : Int = readInt()
+        val y : Int = readInt()
+        val z : Int = readInt()
+        println( (x+y+z)/3.0 )
     }
 }
