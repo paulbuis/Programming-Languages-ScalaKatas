247d706d933f0260a5be73486c13429516182557
Paul Buis <00pebuis@bsu.edu>
2016-07-11 15:33:36 -0400

    Removed non-idiomatic semicolons

--- kata2_1.scala
+++ kata2_1.scala
@@ -1,18 +1,18 @@
 object Kata2_1 {
     
     def read() : Iterable[Int] = {
-        val readLine : () => String = scala.io.StdIn.readLine;
-        val data = scala.collection.mutable.ArrayBuffer.empty[Int];
-        var line : String = readLine();
+        val readLine : () => String = scala.io.StdIn.readLine
+        val data = scala.collection.mutable.ArrayBuffer.empty[Int]
+        var line : String = readLine()
         while (! line.isEmpty ) {
-            data += line.toInt;
-            line = readLine();
+            data += line.toInt
+            line = readLine()
         }
         data 
     }
     
     def main(args: Array[String]) : Unit = {
-        val data = read();
-        println( data.sum / data.size.toDouble );   
+        val data = read()
+        println( data.sum / data.size.toDouble )
     }
 }
