19854ad2d3394d0ae8ae44a86f39469a71dc6ede
Paul Buis <00pebuis@bsu.edu>
2016-07-11 16:55:47 -0400

    Changed to use recursion & memoization
    Also, now uses System.nanoTIme for performance measurement

--- kata4_1.scala
+++ kata4_1.scala
@@ -1,23 +1,35 @@
+
 object Kata4_1 {
 
-    def fib(n : Int) : BigInt = {
-        var a = BigInt(0)
-        var b = BigInt(1)
-        var i = 0
-        while (i<n) {
-            var t = a
-            a = b
-            b = a + t
-            i = i + 1
+    def makeFib() : Int => Long = {
+        var memo = scala.collection.mutable.ArrayBuffer.empty[Long]
+        memo += 0
+        memo += 1
+        def innerFib(n : Int) : Long = {
+            if (n >= memo.length) {
+                memo += innerFib(n-1) + innerFib(n-2)
+            }
+            memo(n)
         }
-        a;
+        innerFib
     }
     
     def main(args: Array[String]) : Unit = {
+        val fib = makeFib()
         println(fib(0))
         println(fib(1))
         println(fib(2))
         println(fib(3))
-        println(fib(4))   
+        println(fib(4))
+
+        var start = System.nanoTime
+        val f50a = fib(50)
+        var end = System.nanoTime
+        printf("computed fib(50)=%,d in %,d nanoseconds\n", f50a, end-start)
+
+	start = System.nanoTime
+        val f50b = fib(50)
+        end = System.nanoTime
+        printf("computed fib(50)=%,d in %,d nanoseconds\n", f50b, end-start)
     }
 }
