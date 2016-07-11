f261cf553448a4e81d37955535a2522e30c18b89
Paul Buis <00pebuis@bsu.edu>
2016-07-11 15:44:11 -0400

    Removed non-idiomatic semicolons

--- kata0_0.scala
+++ kata0_0.scala
@@ -1,10 +1,10 @@
 object Kata0_0 {
     
     def greet(name : String) = {
-        println("Hello, " + name);
+        println("Hello, " + name)
     }
     
     def main(args: Array[String]) = {
-        greet(if (args.length==0) {"world"} else {args(0)});
+        greet(if (args.length==0) {"world"} else {args(0)})
     }
 }
