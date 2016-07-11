d6a26be5a6345f5e27039ba0c332918140e977be
Paul Buis <00pebuis@bsu.edu>
2016-07-11 15:41:05 -0400

    Removed non-idiomatic semicolons

--- kata2_0.scala
+++ kata2_0.scala
@@ -1,24 +1,24 @@
 object Kata2_0 {
     def sum(data: Traversable[Int]) : Int = {
         var result = 0;
-        data.foreach( (datum) => result += datum);
+        data.foreach( (datum) => result += datum)
         result
     }
     
     def read() : Iterable[Int] = {
-        val readLine : () => String = scala.io.StdIn.readLine;
-        val data = scala.collection.mutable.ArrayBuffer.empty[Int];
-        var line : String = readLine();
+        val readLine : () => String = scala.io.StdIn.readLine
+        val data = scala.collection.mutable.ArrayBuffer.empty[Int]
+        var line : String = readLine()
         while (! line.isEmpty ) {
-            data += line.toInt;
-            line = readLine();
+            data += line.toInt
+            line = readLine()
         }
         data 
     }
     
     def main(args: Array[String]) : Unit = {
-        val data = read();
-        val average = sum(data) / data.size.toDouble;
-        println(average);   
+        val data = read()
+        val average = sum(data) / data.size.toDouble
+        println(average)  
     }
 }
